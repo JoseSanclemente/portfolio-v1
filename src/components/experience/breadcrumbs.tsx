a53188e9ce0536ca7aff174bf0ed231ac207c6b2
JoseSanclemente <darkarx06@gmail.com>
2024-08-28 00:18:32 -0500

    Versioning untracked files

--- src/components/experience/breadcrumbs.tsx
+++ src/components/experience/breadcrumbs.tsx
@@ -4,7 +4,10 @@ export default function Breadcrumbs({ itemsList }: BreadcrumsProperties) {
   return (
     <div className="mt-6 flex flex-wrap gap-x-3 gap-y-4 md:gap-x-6">
       {itemsList.map((item) => (
-        <span className="gradient--border-show rounded-3xl px-4 py-3 text-sm leading-none">
+        <span
+          key={item}
+          className="gradient--border-show rounded-3xl px-4 py-3 text-sm leading-none"
+        >
           {item}
         </span>
       ))}
